d720fa813ffa6e07b5d673e99019f08329b2dc54
AnanyaPatel95 <ananyapatel742@gmail.com>
2024-12-06 16:54:18 +0530

    making textfield

--- lib/form.dart
+++ lib/form.dart
@@ -44,6 +44,13 @@ class _MyformState extends State<Myform> {
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(5))),
               ),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5))),
+              ),
+
               SizedBox(
                 height: 10,
               ),
