d4c29e5860ec874e973c577c76d3e7120a99425a
abhay nishad <imabhaynishad@gmail.com>
2024-12-06 16:03:12 +0530

    making form

---
+++ lib/form.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Myform extends StatefulWidget {
+  const Myform({super.key});
+
+  @override
+  State<Myform> createState() => _MyformState();
+}
+
+class _MyformState extends State<Myform> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("data"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Container(
+
+          child: Column(
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5))),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Center(child: Text("Submit")),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
